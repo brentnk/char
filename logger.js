01aaa9782424e738e33ce98d921a37f154cbe649
brentnk <avaquado@gmail.com>
2015-03-02 19:30:11 -0700

    log direct to es

--- logger.js
+++ logger.js
@@ -1,13 +1,23 @@
 var logger = require('bunyan');
+var es     = require('bunyan-elasticsearch');
+var esStream = new es({
+  indexPattern: '[logstash-]YYYY.MM.DD',
+  type: 'logs',
+  host: '127.0.0.1:9200'
+});
+
 var log    = logger.createLogger(
   {
     name:'charBot',
     streams: [
       {
         type: 'rotating-file',
-        path: 'c:/var/logs/charbot.log',
+        path: 'j:/var/logs/charbot.log',
         period: '3d',
         count: 10
+      },
+      {
+        stream: esStream
       }
       // ,
       // {
