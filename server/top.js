f7fbaab63e608f153ba167d7bb825904dcd22736
brentnk <avaquado@gmail.com>
2015-03-11 01:51:10 -0600

    splitting project

--- server/top.js
+++ server/top.js
@@ -1,19 +1,50 @@
 var log = require('../logger');
 
-var top = function(deps, args) {
-  trigger = 'top';
-  dep = ['ircClient'];
+var topExec = function() {
+  twitch.request({},function(err,res) {
+    if(err) {
+      log.err(err);
+    } else if(res) {
+      if('streams' in streams && streams['streams'].length > 0){
+        var top = streams['streams'].slice(0,limit);
+        for(var i = 0; i < top.length; i++) {
+          if ('channel' in top[i]) {
+            deps[ircClient].join('#' + top[i].channel.display_name);
+          }
+        }
+      }
+    } else {
+      log.error('Invalid response from twitch.request');
+    }
+  });
+};
 
-  argDef = {
-    limit: {required: true, numeric: true, gt: 0, default: 3}
-  };
+com.defineCommnad(
+  'top',
+  ['irc'],
+  {limit: {required: true, numeric: true, gt: 0, default: 3}}
+)
+
+var command = function(trigger, definitions, dependencies) {
+  this.trigger = trigger;
+  this.dependencies = dependencies || []; // array of strings
+  this.definition = definitions;
 
   if(!(typeof(args) === 'object')) {
     log.error('Args must be an object');
     return {error:true, message: 'Args mus be an object'};
   }
+}
 
-  var validateArgs = function() {
+Command.prototype.run = function(args) {
+  console.out(args);
+}
+
+Command.prototype.setArgumentDefinition = function(def) {
+  argDef = def;
+}
+
+Command.prototype.validateArgs = function() {
     for(var a in argDef) {
       var def = argDef[a];
       //existence check
@@ -40,24 +71,18 @@ var top = function(deps, args) {
     }
   }
 
-  this.trigger = function(key) { return key === trigger; };
-
-  this.fire = function() {
-    twitch.request({},function(err,res) {
-      if(err) {
-        log.err(err);
-      } else if(res) {
-        if('streams' in streams && streams['streams'].length > 0){
-          var top = streams['streams'].slice(0,limit);
-          for(var i = 0; i < top.length; i++) {
-            if ('channel' in top[i]) {
-              deps[ircClient].join('#' + top[i].channel.display_name);
-            }
-          }
-        }
-      } else {
-        log.error('Invalid response from twitch.request');
-      }
-    })
-  };
+Command.prototype.help = function() {
+  return 'default help';
 };
+
+Command.prototype.trigger = function(key) {
+  return key === trigger;
+};
+
+Command.prototype.execute = function() {
+  if(this.validateArgs()) {
+    this.run(args);
+  }
+};
+
+module.exports = exports = top;
